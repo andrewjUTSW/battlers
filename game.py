915731d65b638aa6bb64fe06f53dda72290c7518
williamj <willja.email>
2024-12-21 08:55:21 -0600

    updates

--- game.py
+++ game.py
@@ -16,7 +16,16 @@ class FightingGame:
         glMatrixMode(GL_PROJECTION)
         gluPerspective(45, (width/height), 0.1, 50.0)
         glMatrixMode(GL_MODELVIEW)
-        glTranslatef(0.0, 0.0, -10)
+        # Move camera back and up slightly to see the scene better
+        glTranslatef(0.0, -1.0, -15.0)
+        
+        # Enable depth testing
+        glEnable(GL_DEPTH_TEST)
+        
+        # Add basic lighting
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glLight(GL_LIGHT0, GL_POSITION, (5, 5, 5, 1))
 
         # Game state
         self.running = True
@@ -35,20 +44,59 @@ class FightingGame:
         pass
 
     def draw(self):
-        # Clear the screen
+        # Clear the screen and set background color to dark blue
+        glClearColor(0.1, 0.1, 0.2, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
-
-        # Basic ground plane (temporary)
+        
+        # Move camera back and up slightly
+        glTranslatef(0.0, -1.0, -15.0)
+        
+        # Draw ground plane (larger and with a more visible color)
         glBegin(GL_QUADS)
-        glColor3f(0.5, 0.5, 0.5)
-        glVertex3f(-5, -2, -5)
-        glVertex3f(-5, -2, 5)
-        glVertex3f(5, -2, 5)
-        glVertex3f(5, -2, -5)
+        glColor3f(0.2, 0.5, 0.2)  # Green color for ground
+        glVertex3f(-10, -2, -10)
+        glVertex3f(-10, -2, 10)
+        glVertex3f(10, -2, 10)
+        glVertex3f(10, -2, -10)
         glEnd()
 
+        # Draw a reference cube to help orient ourselves
+        self.draw_cube(0, 0, 0)
+        
         pygame.display.flip()
+
+    def draw_cube(self, x, y, z):
+        # Helper function to draw a small cube
+        glPushMatrix()
+        glTranslatef(x, y, z)
+        glBegin(GL_QUADS)
+        # Front face (red)
+        glColor3f(1.0, 0.0, 0.0)
+        glVertex3f(-0.5, -0.5, 0.5)
+        glVertex3f(0.5, -0.5, 0.5)
+        glVertex3f(0.5, 0.5, 0.5)
+        glVertex3f(-0.5, 0.5, 0.5)
+        # Back face (blue)
+        glColor3f(0.0, 0.0, 1.0)
+        glVertex3f(-0.5, -0.5, -0.5)
+        glVertex3f(-0.5, 0.5, -0.5)
+        glVertex3f(0.5, 0.5, -0.5)
+        glVertex3f(0.5, -0.5, -0.5)
+        # Top face (green)
+        glColor3f(0.0, 1.0, 0.0)
+        glVertex3f(-0.5, 0.5, -0.5)
+        glVertex3f(-0.5, 0.5, 0.5)
+        glVertex3f(0.5, 0.5, 0.5)
+        glVertex3f(0.5, 0.5, -0.5)
+        # Bottom face (yellow)
+        glColor3f(1.0, 1.0, 0.0)
+        glVertex3f(-0.5, -0.5, -0.5)
+        glVertex3f(0.5, -0.5, -0.5)
+        glVertex3f(0.5, -0.5, 0.5)
+        glVertex3f(-0.5, -0.5, 0.5)
+        glEnd()
+        glPopMatrix()
 
     def run(self):
         while self.running:
